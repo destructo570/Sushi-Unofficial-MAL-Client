751fedd17663c3e0671b642360a28158a0fc62dd
Vishal Kashi <dev.vishalkashi@gmail.com>
2020-10-08 19:37:29 +0530

    Initial commit

---
+++ Sushi/app/src/main/java/com/destructo/sushi_mal/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.destructo.sushi_mal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
