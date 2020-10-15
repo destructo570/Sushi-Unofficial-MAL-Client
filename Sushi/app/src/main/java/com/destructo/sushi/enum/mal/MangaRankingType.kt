59510476d5d65285e6ba797432031a0212c2abe3
Vishal Kashi <dev.vishalkashi@gmail.com>
2020-10-16 04:40:33 +0530

    add ranking type filter in manga section

---
+++ Sushi/app/src/main/java/com/destructo/sushi/enum/mal/MangaRankingType.kt
@@ -0,0 +1,13 @@
+package com.destructo.sushi.enum.mal
+
+enum class MangaRankingType(val value:String) {
+    ALL("all"),
+    MANGA("manga"),
+    NOVELS("novels"),
+    ONESHOTS("oneshots"),
+    DOUJIN("doujin"),
+    MANHWA("manhwa"),
+    MANHUA("manhua"),
+    BY_POPULARITY("bypopularity"),
+    FAVORITE("favorite")
+}
--- Sushi/app/src/main/java/com/destructo/sushi/ui/manga/MangaFragment.kt
+++ Sushi/app/src/main/java/com/destructo/sushi/ui/manga/MangaFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -12,21 +15,23 @@ import com.destructo.sushi.R
 import com.destructo.sushi.databinding.FragmentAnimeBinding
 import com.destructo.sushi.databinding.FragmentMangaBinding
 import com.destructo.sushi.enum.TopSubtype
+import com.destructo.sushi.enum.mal.MangaRankingType
 import com.destructo.sushi.ui.anime.topAnime.TopAnimeAdapter
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MangaFragment : Fragment() {
+class MangaFragment : Fragment(),AdapterView.OnItemSelectedListener {
 
     private val mangaViewModel:MangaViewModel by viewModels()
 
     private lateinit var binding:FragmentMangaBinding
     private lateinit var mangaRecycler:RecyclerView
     private lateinit var mangaAdapter:MangaAdapter
+    private lateinit var mangaTypeSpinner:Spinner
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        mangaViewModel.getTopMangaList("all","500",null)
+        mangaViewModel.getTopMangaList(MangaRankingType.ALL.value,"500",null)
     }
 
     override fun onCreateView(
@@ -36,6 +41,14 @@ class MangaFragment : Fragment() {
         binding = FragmentMangaBinding.inflate(inflater,container,false).apply {
             lifecycleOwner = viewLifecycleOwner
         }
+        mangaTypeSpinner = binding.mangaRankingSpinner
+        context?.let { ArrayAdapter.createFromResource(
+            it,R.array.manga_ranking_type,android.R.layout.simple_spinner_item).also {adapter->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            mangaTypeSpinner.adapter = adapter
+            mangaTypeSpinner.onItemSelectedListener = this
+        } }
+
         mangaRecycler = binding.mangaRecyclerMain
         mangaRecycler.layoutManager = GridLayoutManager(context, 3)
 
@@ -54,4 +67,24 @@ class MangaFragment : Fragment() {
             }
         }
     }
-}
+
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, pos: Int, id: Long) {
+        when(parent?.getItemAtPosition(pos).toString()){
+            "All" -> {mangaViewModel.getTopMangaList(MangaRankingType.ALL.value,"500",null)}
+            "Manga" -> {mangaViewModel.getTopMangaList(MangaRankingType.MANGA.value,"500",null)}
+            "Novels" -> {mangaViewModel.getTopMangaList(MangaRankingType.NOVELS.value,"500",null)}
+            "Oneshots" -> {mangaViewModel.getTopMangaList(MangaRankingType.ONESHOTS.value,"500",null)}
+            "Doujin" -> {mangaViewModel.getTopMangaList(MangaRankingType.DOUJIN.value,"500",null)}
+            "Manhwa" -> {mangaViewModel.getTopMangaList(MangaRankingType.MANHWA.value,"500",null)}
+            "Manhua" -> {mangaViewModel.getTopMangaList(MangaRankingType.MANHUA.value,"500",null)}
+            "By Popularity" -> {mangaViewModel.getTopMangaList(MangaRankingType.BY_POPULARITY.value,"500",null)}
+            "Favorite" -> {mangaViewModel.getTopMangaList(MangaRankingType.FAVORITE.value,"500",null)}
+        }
+    }
+
+    override fun onNothingSelected(p0: AdapterView<*>?) {
+        mangaViewModel.getTopMangaList(MangaRankingType.ALL.value,"500",null)
+    }
+
+    }
+
