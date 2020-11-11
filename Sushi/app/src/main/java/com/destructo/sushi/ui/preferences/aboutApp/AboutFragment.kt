2b50799938f20e836ce81a61a0501a24c0bf2fc3
Vishal Kashi <dev.vishalkashi@gmail.com>
2020-11-11 21:10:33 +0530

    Finish about app section

--- Sushi/app/src/main/java/com/destructo/sushi/ui/preferences/aboutApp/AboutFragment.kt
+++ Sushi/app/src/main/java/com/destructo/sushi/ui/preferences/aboutApp/AboutFragment.kt
@@ -1,21 +1,28 @@
 package com.destructo.sushi.ui.preferences.aboutApp
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
 import com.destructo.sushi.R
 import com.destructo.sushi.databinding.FragmentAboutBinding
 import com.destructo.sushi.databinding.FragmentSettingsBinding
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_about.view.*
 
 class AboutFragment : Fragment() {
 
     private lateinit var binding: FragmentAboutBinding
     private lateinit var toolbar: Toolbar
+    private lateinit var twitterButton:Button
+    private lateinit var dribbbleButton:Button
+    private lateinit var discordButton:Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,6 +36,19 @@ class AboutFragment : Fragment() {
             lifecycleOwner = viewLifecycleOwner
         }
         toolbar = binding.toolbar
+        twitterButton = binding.devTwitterButton
+        dribbbleButton = binding.devDribbbleButton
+        discordButton = binding.devDiscordButton
+
+        twitterButton.setOnClickListener {
+            openUrl(getString(R.string.social_twitter_link))
+        }
+        dribbbleButton.setOnClickListener {
+            openUrl(getString(R.string.social_dribbble_link))
+        }
+        discordButton.setOnClickListener {
+            openUrl(getString(R.string.social_discord_link))
+        }
 
         return binding.root
     }
@@ -42,6 +62,12 @@ class AboutFragment : Fragment() {
         toolbar.setNavigationOnClickListener {
             activity?.drawer_layout?.openDrawer(GravityCompat.START)
         }
+    }
+
+    private fun openUrl(url: String) {
+        val intent = Intent(Intent.ACTION_VIEW)
+        intent.data = Uri.parse(url)
+        startActivity(intent)
     }
 
 }
