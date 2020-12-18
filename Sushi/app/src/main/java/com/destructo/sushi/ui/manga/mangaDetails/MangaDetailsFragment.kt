1fbfa66a5aeb28d994e7c1ad080ae5a45b19df77
Vishal Kashi <dev.vishalkashi@gmail.com>
2020-12-18 08:38:43 +0530

    Fix launcher icon not showing right icon

--- Sushi/app/src/main/java/com/destructo/sushi/ui/manga/mangaDetails/MangaDetailsFragment.kt
+++ Sushi/app/src/main/java/com/destructo/sushi/ui/manga/mangaDetails/MangaDetailsFragment.kt
@@ -205,7 +205,6 @@ class MangaDetailsFragment : Fragment(), MangaUpdateListener, AppBarLayout.OnOff
         })
 
         reviewAdapter = MangaReviewAdapter(MangaReviewListener {
-            it?.let { it.url?.let { it1 -> openUrl(it1) } }
             it?.let {
                 val reviewDialog = MangaReviewBottomSheetFragment.newInstance(it)
                 reviewDialog.show(childFragmentManager, "manga_review_dialog")
