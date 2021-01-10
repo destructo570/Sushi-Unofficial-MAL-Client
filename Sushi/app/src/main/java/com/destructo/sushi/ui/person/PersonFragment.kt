cc66a6b7a2ba3b0cf05e5ae0fbfd615054df2233
Vishal Kashi <dev.vishalkashi@gmail.com>
2021-01-10 20:59:55 +0530

    Refactor code

--- Sushi/app/src/main/java/com/destructo/sushi/ui/person/PersonFragment.kt
+++ Sushi/app/src/main/java/com/destructo/sushi/ui/person/PersonFragment.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.graphics.drawable.Drawable
 import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
@@ -107,7 +106,7 @@ class PersonFragment : Fragment(), AppBarLayout.OnOffsetChangedListener {
                     setupViewPager(fragmentList)
                 }
                 Status.ERROR -> {
-                    Log.e("PersonFragment", "Error: ${resource.message}")
+                    
                 }
 
             }
