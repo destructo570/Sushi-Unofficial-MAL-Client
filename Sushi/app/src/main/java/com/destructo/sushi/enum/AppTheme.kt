64dca9b5415f0d3b4db93cae499e50632ac2875e
Vishal Kashi <dev.vishalkashi@gmail.com>
2021-02-15 12:27:33 +0530

    Fix profile stats card color issue in dark theme

--- Sushi/app/src/main/java/com/destructo/sushi/enum/AppTheme.kt
+++ Sushi/app/src/main/java/com/destructo/sushi/enum/AppTheme.kt
@@ -4,12 +4,5 @@ enum class AppTheme(val value: String) {
     LIGHT("default_theme"),
     DARK("dark_theme"),
     NIGHT("night_theme"),
-    NIGHT_LIME("night_lime_theme"),
-    NIGHT_SAKURA("night_sakura_theme"),
-    NIGHT_CYAN("night_cyan_theme"),
     AMOLED("amoled_theme"),
-    AMOLED_LIME("amoled_lime_theme"),
-    AMOLED_SAKURA("amoled_sakura_theme"),
-    AMOLED_CYAN("amoled_cyan_theme"),
-
 }
