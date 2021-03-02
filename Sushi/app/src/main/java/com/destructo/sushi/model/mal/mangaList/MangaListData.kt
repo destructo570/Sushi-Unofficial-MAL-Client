1c7f9a1287d4c7a9940afa49e3c3eead35667da0
Vishal Kashi <dev.vishalkashi@gmail.com>
2021-03-02 11:18:19 +0530

    Fix shuffling of items in manga tab in search fragment

--- Sushi/app/src/main/java/com/destructo/sushi/model/mal/mangaList/MangaListData.kt
+++ Sushi/app/src/main/java/com/destructo/sushi/model/mal/mangaList/MangaListData.kt
@@ -2,11 +2,14 @@ package com.destructo.sushi.model.mal.mangaList
 
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.destructo.sushi.model.mal.manga.Manga
 import com.squareup.moshi.Json
 
-@Entity(tableName = "search_manga_list", primaryKeys = ["manga"])
+@Entity(tableName = "search_manga_list")
 data class MangaListData(
     @Json(name = "node")
-    val manga: Manga
+    val manga: Manga,
+    @PrimaryKey(autoGenerate = true)
+    val id: Int?=null
 )
